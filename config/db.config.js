f672ae788a52ddec83a9cc66f0b3a05b2d4f5fd3
miguelbachi5a <miguelbachi5a@gmail.com>
2020-08-21 17:29:39 +0200

    MONGODB URI

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect( 'mongodb://127.0.0.1:27017/project-management', { useNewUrlParser: true })
+mongoose.connect( process.env.MONGOLAB_URI, { useNewUrlParser: true })
   .then(() => console.info('Successfully connected to the database Project-Management-Tool'))
   .catch(error => console.error('An error ocurred trying to connect to de database Project-Management-Tool', error))
 
