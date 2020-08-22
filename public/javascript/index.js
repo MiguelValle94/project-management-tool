e7b1115908ee79e9c5c82ba29e7e251d292df19f
miguelbachi5a <miguelbachi5a@gmail.com>
2020-08-22 11:15:49 +0200

    Axios Heroku

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,7 +1,7 @@
 function like(event) {
     const button = event.currentTarget
 
-	axios.post(`http://localhost:3000/projects/${button.id}/like`)
+	axios.post(`https://project-management-tool-miguel.herokuapp.com/projects/${button.id}/like`)
 		.then(res => {
 			const add = res.data.like
 			button.querySelector('.likes-count').innerText = Number(button.querySelector('.likes-count').innerText) + add
