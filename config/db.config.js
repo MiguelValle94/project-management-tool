755d40fdc0a1b2e7f9c22e2f13c92f5321f6cd39
miguelbachi5a <miguelbachi5a@gmail.com>
2020-08-22 12:34:38 +0200

    Seeds

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
+const MONGOLAB_URI = process.env.MONGOLAB_URI || 'mongodb://127.0.0.1:27017/project-management-tool'
 
-mongoose.connect( process.env.MONGOLAB_URI, { useNewUrlParser: true })
+mongoose.connect( MONGOLAB_URI, { useNewUrlParser: true })
   .then(() => console.info('Successfully connected to the database Project-Management-Tool'))
   .catch(error => console.error('An error ocurred trying to connect to de database Project-Management-Tool', error))
 
